59d80be89622084d8e724f1d5f7623b8eccab553
XargonWan <XargonWan@gmail.com>
2023-09-27 20:54:06 +0200

    Updated controller_generator script

--- automation_tools/controller_generator.sh
+++ automation_tools/controller_generator.sh
@@ -1,56 +1,71 @@
 #!/bin/bash
 
-# STILL BROKEN
+# STILL BROKEN, DON'T USE ME
 
 # This script is generating the controller config sourced from Neptune (Steam Deck controller)
 
-NEPTUNE=emu-configs/defaults/retrodeck/RetroDECK_controller_config.vdf
-read -r -d '' RADIAL <<EOF
-            HELLO
-            I
-            AM
-            THE
-            RADIAL
-EOF
+extract_radial() {
+  local RADIAL=$(<emu-configs/defaults/retrodeck/RetroDECK_controller_config.vdf)
+  local mode_line='"mode"[[:space:]]*"radial_menu"'
+  local counter=0
+  local new_radial=""
+  local start_reading=false
 
-# GENERIC
-GENERIC="${NEPTUNE%.vdf}_GENERIC.vdf"
-rm -f $GENERIC
-cp $NEPTUNE $GENERIC
-controller_type="controller_generic"
-controller_caps="1573887"
-sed -i "s/controller_neptune/$controller_type/g" "$GENERIC" > /dev/null
-sed -i "s/23117823/$controller_caps/g" "$GENERIC" > /dev/null
+  # Debug: Display the mode line for troubleshooting
+  echo "Searching for: $mode_line"
+
+  # Read the RADIAL variable line by line
+  while IFS= read -r line; do
+    # Start reading when the line matches the mode line
+    if [[ "$line" =~ $mode_line ]]; then
+      start_reading=true
+      # Debug: Indicate that we started reading
+      echo "Started reading..."
+      continue  # Skip adding the mode line itself
+    fi
+
+    # Continue reading when start_reading is true
+    if [ "$start_reading" == true ]; then
+      # Increment/decrement counter for '{' and '}'
+      if [[ "$line" == *"{"* ]]; then
+        ((counter++))
+
+        # After finding the first '{', delete that '{' and everything before it
+        if [ $counter -eq 1 ]; then
+          new_radial="${line#*{ }"
+        fi
+      elif [[ "$line" == *"}"* ]]; then
+        ((counter--))
+
+        # When the counter reaches 0, stop reading and delete the rest
+        if [ $counter -eq 0 ]; then
+          # Debug: Indicate that we stopped reading
+          echo "Stopped reading..."
+          break
+        fi
+      else
+        # Append the line to new_radial when counter is not 0
+        new_radial="$new_radial$line"$'\n'
+      fi
+    fi
+  done <<< "$RADIAL"
+
+  # Print the modified content
+  echo -n "$new_radial"
+}
+
+# Call the function to extract RADIAL
+RADIAL=$(extract_radial)
+
+echo "$RADIAL"
+
 
 # PS3
-RADIAL_CONTROL_ID=17
-PS3="${NEPTUNE%.vdf}_PS3.vdf"
-rm -f $PS3
-cp $NEPTUNE $PS3
-controller_type="controller_ps3"
-controller_caps="1590271"
-sed -i "s/controller_neptune/$controller_type/g" "$PS3" > /dev/null
-sed -i "s/23117823/$controller_caps/g" "$PS3" > /dev/null
-
-# Define the pattern with the actual value
-pattern="^id[[:space:]]+\"$RADIAL_CONTROL_ID\"$"
-
-# Find the line number where the pattern occurs
-line_number=$(awk -v pat="$pattern" '$0 ~ pat {print NR}' $PS3)
-echo "DEBUG: $line_number"
-
-if [ ! -z "$line_number" ]; then
-    # Find the start of the "inputs" section by searching for the line containing "inputs"
-    start_line=$(awk -v line="$line_number" 'NR < line && $0 ~ /"inputs"/ {print NR}' $PS3)
-    echo "DEBUG: $start_line"
-    
-    if [ ! -z "$start_line" ]; then
-        # Find the end of the "inputs" section by searching for the next "}" after "inputs"
-        end_line=$(awk -v start="$start_line" '$0 ~ /}/ && NR > start {print NR; exit}' $PS3)
-        
-        if [ ! -z "$end_line" ]; then
-            # Replace the content inside the "inputs" section with the value of $RADIAL_CONTROL_ID
-            sed -i "$start_line,$end_line{s/.*/$RADIAL_CONTROL_ID/}" $PS3
-        fi
-    fi
-fi
+# RADIAL_CONTROL_ID=17
+# PS3="${NEPTUNE%.vdf}_PS3.vdf"
+# rm -f $PS3
+# cp $NEPTUNE $PS3
+# controller_type="controller_ps3"
+# controller_caps="1590271"
+# sed -i "s/controller_neptune/$controller_type/g" "$PS3" > /dev/null
+# sed -i "s/23117823/$controller_caps/g" "$PS3" > /dev/null
